682daa09200511ae3754132aa7e11c359f362c45
navire-JP <navire.jp@gmail.com>
2026-01-17 23:06:52 +0100

    main patch

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import APP_NAME, APP_VERSION, CORS_ORIGINS
 from app.db.database import Base, engine
-from app.db import models   # ✅ AJOUTE CETTE LIGNE (important)
+from app.db import models 
 from app.routers.auth import router as auth_router
 
 app = FastAPI(title=APP_NAME, version=APP_VERSION)
@@ -10,10 +10,11 @@ app = FastAPI(title=APP_NAME, version=APP_VERSION)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS if CORS_ORIGINS else ["*"],
-    allow_credentials=True,
+    allow_credentials=False,  
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
